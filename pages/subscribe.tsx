1abdb9321ebdf7616bb02cd08dfc6194f6069359
Pasha Khomchenko <pavel.kh005@gmail.com>
2022-07-14 18:04:40 -0400

    add name param

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -4,9 +4,11 @@ import { useRouter } from "next/router";
 const Subscribe = () => {
   const { query } = useRouter();
   const [success, setSuccess] = useState(false);
+  const [name, setName] = useState("");
   useEffect(() => {
     query.success && setSuccess(true);
-  }, [query.success]);
+    query.name && setName(String(query.name));
+  }, [query]);
   return (
     <div className="h-screen w-screen font-Michroma flex flex-col justify-center items-center">
       {success ? (
@@ -14,7 +16,7 @@ const Subscribe = () => {
           <h1 className="text-xl">Payment was successful</h1>
           <a
             className="text-xl underline"
-            href="https://infinitedegrees.xyz/subscribe?success=true"
+            href={`https://infinitedegrees.xyz/subscribe?success=true&name=${name}`}
           >
             Return to the app
           </a>
