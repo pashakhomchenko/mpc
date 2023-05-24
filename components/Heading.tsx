81b281a7201d42cc41f60dae8628db5ac951fed6
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-05-24 15:38:17 -0700

    add substack

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -3,7 +3,7 @@ const Heading = () => {
     <h1 className="font-Michroma text-4xl text-black select-none">
       Pasha
       <a
-        href="/blog"
+        href="https://pashakhomchenko.substack.com/"
         target="_blank"
         className="inline-flex bg-gray-light text-red drop-shadow-lg text-2xl py-2 px-6 ml-6 mb-1"
       >
