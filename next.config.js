58e38a3a6d527a90194f107767364f1372ef6937
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-08-09 16:14:47 -0700

    remove redirect

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination:
-          "https://pashakhomchenko.notion.site/Pasha-Khomchenko-a392f4c239cc408bbcee6c5b5480c1ca?pvs=4",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
