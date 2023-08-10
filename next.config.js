1aa4474ccf1a3ae51365be4480d71865f4034c26
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-08-09 17:37:38 -0700

    bring it back

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://pashakhomchenko.notion.site/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
